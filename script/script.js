4f590efaa6739b537c3b327ecc70c282c62e62fa
Your Name <you@example.com>
2025-05-23 14:13:44 +0330

    create object model

---
+++ script/script.js
@@ -0,0 +1,21 @@
+let mails=[
+    {
+        from:"Borge Refsnes",
+        subject:"Remember Me", 
+        body:"<p>Hello, i just wanted to let you know that i'll be home at lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p><p>Best Regards, <br>Borge Refsnes</p>",  
+        date:"Sep 27, 2015" , 
+        emailpic:"images\avatar3.png"},
+    {
+        from:"Jane Doe", 
+        subject:"None", 
+        body:" <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p><p>Forever yours,<br>Jane</p>", 
+        date:"Sep 25, 2015" , 
+        emailpic:"images\avatar5.png"
+    },
+    {
+        from:"John Doe",
+        subject:"None",
+        body:"<p>Welcome.</p><p>That's it!</p>", 
+        date:"Sep 23, 2015" , 
+        emailpic:"images\avatar2.png"}
+];
