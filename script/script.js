addab25a6cca06ef1b308819c3ec2966da6949be
Your Name <you@example.com>
2025-05-23 15:50:05 +0330

    use substring

--- script/script.js
+++ script/script.js
@@ -4,22 +4,30 @@ let mails=[
         subject:"Remember Me", 
         body:"<p>Hello, i just wanted to let you know that i'll be home at lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p><p>Best Regards, <br>Borge Refsnes</p>",  
         date:"Sep 27, 2015" , 
-        emailpic:"images\avatar3.png"},
+        emailpic:"/images/avatar3.png"},
     {
         from:"Jane Doe", 
         subject:"None", 
         body:" <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p><p>Forever yours,<br>Jane</p>", 
         date:"Sep 25, 2015" , 
-        emailpic:"images\avatar5.png"
+        emailpic:"/images/avatar5.png"
     },
     {
         from:"John Doe",
         subject:"None",
         body:"<p>Welcome.</p><p>That's it!</p>", 
         date:"Sep 23, 2015" , 
-        emailpic:"images\avatar2.png"}
+        emailpic:"/images/avatar2.png"}
 ];
 
-document.getElementById("fullname1").innerText=mails[0].from;
-document.getElementById("fullname2").innerText=mails[1].from;
-document.getElementById("fullname3").innerText=mails[2].from;
+
+let images=document.getElementsByClassName("emailtombnail");
+let fullnames=document.getElementsByClassName("fullname");
+let subjects= document.getElementsByClassName("subject");
+let mailbodies= document.getElementsByClassName("mailbody");
+for (let index = 0; index < mails.length; index++) {
+   fullnames[index].innerText=mails[index].from
+    images[index].src=mails[index].emailpic;
+    subjects[index].innerText= mails[index].subject;
+    mailbodies[index].innerHTML=mails[index].body.substring(0,60) + "...";
+}
